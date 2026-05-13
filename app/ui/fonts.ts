c4d29d31cb49c3ee1cb044970300e43335f10898
Cauê Moraes <kauegraphics@gmail.com>
2026-05-13 15:49:02 -0300

    Chaps 1-5 of next.js tut

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Inter } from 'next/font/google';
+import { Lusitana } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+export const lusi = Lusitana({
+  weight: ['400', '700'], // Specify weights as strings in an array
+  subsets: ['latin'],     // Add 'latin' or other supported subsets
+});
