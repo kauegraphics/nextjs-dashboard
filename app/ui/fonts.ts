fcfdae9d8014c4dd7c86f81cbf31026cf4ebb2ba
Cauê Moraes <kauegraphics@gmail.com>
2026-05-13 16:22:04 -0300

    Corrected Lusitana font bug

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -2,7 +2,7 @@ import { Inter } from 'next/font/google';
 import { Lusitana } from 'next/font/google';
  
 export const inter = Inter({ subsets: ['latin'] });
-export const lusi = Lusitana({
+export const lusitana = Lusitana({
   weight: ['400', '700'], // Specify weights as strings in an array
   subsets: ['latin'],     // Add 'latin' or other supported subsets
 });
